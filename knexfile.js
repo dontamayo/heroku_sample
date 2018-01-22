9093a3de53d3cce185ce5f86463a8d60dce17dff
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-01-22 14:24:10 -0500

    Add database config and connection creation

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// throw an error for missing application mode.
+if (process.env.APP_MODE === undefined) {
+ throw Error('APP_MODE not set in application environment');
+}
+
+module.exports = {
+ development: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL
+ },
+ production: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL
+ }
+}
